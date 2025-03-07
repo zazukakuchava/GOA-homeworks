5f8656d4eedafc98c9844c598685b4dbe979d855
zazukakuchava <svanidzeshorena7@gmail.com>
2025-03-07 10:56:24 -0800

    fifth commit

---
+++ Day 04/classwork/lesson 4.py
@@ -0,0 +1,33 @@
+print("zaza")
+print(15)
+print(10.9)
+
+name=("ronaldinio")
+print(name)
+
+name=("messi")
+print(name)
+
+name=("jakson")
+print(name)
+
+name=("xinkali")
+print("eat")
+
+name=("qababi")
+print("eat")
+
+
+name=("supi")
+print("eat")
+
+name=("mwvadi")
+print("eat")
+
+
+
+name = "group59"
+print(10)
+age=(100)
+print(10.0)
+print("goa best")
---
+++ Day 04/homework/lesson 4.py
@@ -0,0 +1,61 @@
+name="zaza"
+name="nika"
+name="guga"
+name="gio"
+name="toma"
+print(name)
+
+
+
+
+year="10"
+year="19"
+year="100000000"
+year="100000002"
+year="100000001"
+print(year)
+
+
+
+
+surname="kuchava"
+surname="gabunia"
+surname="svanidze"
+surname="keshelava"
+surname="chankvetadze"
+print(surname)
+
+
+
+# 1
+num1 = "one"  #შეცდომა იყო რომ პრჭყალების მაგივრად ეწერა სხვა რაღაცა.
+print(num1)
+# 2
+num2 = "two"  #შეცდომა იყო რომ პრინტი არ ქონდა და ფრჩხილები ქონდა.
+print(num2)  
+# 3
+num3 = "three" # შეცდომა იყო რომ დიდი ასოთი იყო დაწყებული.
+print(num3)
+# 4
+num4 = "number4" #შეცდომა იყო რომ ორივე პრჭყალი განსხვავებული იყო ერთმანეთისგან
+print(num4)
+# 5
+num5 = "five"  #შეცდომა იყო წერაში
+print(num5)
+
+
+num1=15
+num2=16
+num3=17
+num4=18
+num5=num1+num2+num3+num4
+print(num5)
+
+
+
+
+
+
+
+
+
